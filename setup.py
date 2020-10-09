c6d9535a52b43461c4f378ef28da3e2dc7d875cd
Luanee <62288132+Luanee@users.noreply.github.com>
2020-10-09 15:17:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,22 @@
 from distutils.core import setup
 
-setup(name="scrapeasy",
-      packages=["scrapeasy"],
-      version="0.12",
+setup(name="simplysql",
+      packages=["simplysql"],
+      version="0.1",
       license="MIT",
-      description="Scraping in python made easy - receive the content you like in just one line of code!",
+      description="Simplified editing of SQL databases (sqlite3) without having to deal with queries!",
       author="Pit Nahrstedt",
-      author_email="joel.barmettler@uzh.ch",
-      url="https://github.com/joelbarmettlerUZH/Scrapeasy",
-      download_url="https://github.com/joelbarmettlerUZH/Scrapeasy/archive/pypi-0_1_3.tar.gz",
-      keywords=["scraping", "easy", "scraper", "website", "download", "links", "images", "videos"],
-      install_requires=[
-          "validators",
-          "beautifulsoup4",
-      ],
-      classifiers=[  # Optional
-          # How mature is this project? Common values are
-          #   3 - Alpha
-          #   4 - Beta
-          #   5 - Production/Stable
-          "Development Status :: 3 - Alpha",
-
-          # Indicate who your project is intended for
-          "Intended Audience :: Developers",
-          "Topic :: Software Development :: Build Tools",
-
-          # Pick your license as you wish
-          "License :: OSI Approved :: MIT License",
-
-          # Specify the Python versions you support here. In particular, ensure
-          # that you indicate whether you support Python 2, Python 3 or both.
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-      ],
+      author_email="pit.nahrstedt@web.de",
+      url="https://github.com/Luanee/SimplySQL",
+      # download_url="url.tar.gz",
+      keywords=["sql", "easy", "queries", "databases", "sqlite3"],
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "Topic :: Software Development :: Build Tools",
+                   "License :: OSI Approved :: MIT License",
+                   # TODO: Check compatibility
+                   # "Programming Language :: Python :: 3",
+                   # "Programming Language :: Python :: 3.4",
+                   # "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.7"],
       )
