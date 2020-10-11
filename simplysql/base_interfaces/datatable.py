6f7f12d35fe9489e07dc33dc3c6a1fdcd73748fb
Pit Nahrstedt <62288132+Luanee@users.noreply.github.com>
2020-10-11 14:40:28 +0200

    Added typehints

--- simplysql/base_interfaces/datatable.py
+++ simplysql/base_interfaces/datatable.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3.7.6
-
+from typing import List, Dict, Tuple, Any, Union
 from simplysql.vector_interfaces.datarows import DataRows
 from simplysql.vector_interfaces.datarow import DataRow
 from simplysql.vector_interfaces.datacolumns import DataColumns
@@ -7,7 +7,7 @@ from simplysql.vector_interfaces.datacolumn import DataColumn
 
 
 class DataTable(object):
-    def __init__(self, parent=None, id=0, name=None, header=None, htypes=None, exists=False):
+    def __init__(self, parent=None, id: int = 0, name: str = None, header: List[str] = None, htypes: List[str] = None, exists: bool = False):
         self._rowid = None
         self._id = id
         self._name = name
@@ -18,34 +18,43 @@ class DataTable(object):
         if not exists:
             self._create()
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def __repr__(self):
+        return "DataTable(name={})".format(self.name)
+
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @property
-    def index(self):
+    def index(self) -> int:
         return self._id
 
     @property
-    def empty(self):
+    def empty(self) -> bool:
         if self.count_rows() >= 1:
             return False
         return True
 
     @property
-    def max_row(self):
+    def max_row(self) -> int:
         return self.count_rows()
 
     @property
-    def max_column(self):
+    def max_column(self) -> int:
         return self.count_columns()
 
     @property
-    def shape(self):
+    def shape(self) -> Tuple[int, int]:
         return (self.max_row, self.max_column)
 
     @property
-    def exists(self):
+    def exists(self) -> bool:
         return self._exists
 
     @property
@@ -53,38 +62,38 @@ class DataTable(object):
         return self._parent
 
     @property
-    def data(self):
+    def data(self) -> List[Tuple[Any]]:
         return [tuple(list(row)[1:]) for row in self._get_table()]
 
     @property
-    def rowids(self):
+    def rowids(self) -> List[int]:
         return [row[0] for row in self._get_table()]
 
-    def header(self):
+    def header(self) -> List[str]:
         return self._header
 
-    def htypes(self):
+    def htypes(self) -> List[str]:
         return self._htypes
 
-    def count_rows(self):
+    def count_rows(self) -> int:
         return len(self.data)
 
-    def count_columns(self):
+    def count_columns(self) -> int:
         return len(self.header())
 
-    def as_table(self, with_rowid=False):
+    def as_table(self, with_rowid: bool = False) -> str:
         return self._to_string(with_rowid=with_rowid)
 
-    def as_dict(self):
-        return dict(zip(self.header(), list(map(list, zip(*self.data)))))
-
-    def as_list(self):
+    def as_list(self) -> List[List[Any]]:
         return list(map(list, self.data))
 
-    def as_tuple(self):
+    def as_dict(self) -> Dict[str, List[Any]]:
+        return dict(zip(self.header(), list(map(list, zip(*self.data)))))
+
+    def as_tuple(self) -> Tuple[Tuple[Any]]:
         return tuple(self.data)
 
-    def rename_column(self, old_name, new_name):
+    def rename_column(self, old_name: str, new_name: str) -> None:
         """
         Short summary.
 
@@ -99,10 +108,10 @@ class DataTable(object):
         self.parent.query(sql)
         self.parent._rename_table_column(self.index, old_name, new_name)
 
-    def columns(self):
+    def columns(self) -> DataColumns:
         return DataColumns(self.header(), list(self.data), self)
 
-    def column(self, index):
+    def column(self, index: Union[int, str]) -> DataColumn:
         if not isinstance(index, int):
             index = self.header().index(index)
 
@@ -114,10 +123,10 @@ class DataTable(object):
         return DataColumn(self.header()[index], column,
                           index=index, parent=self)
 
-    def rows(self):
+    def rows(self) -> DataRows:
         return DataRows(self.header(), list(self.data), self)
 
-    def row(self, index):
+    def row(self, index: Union[int, str]) -> DataRow:
         if not abs(index) <= self.count_rows():
             raise IndexError(f"DataTable has no row index {index}")
 
@@ -128,7 +137,7 @@ class DataTable(object):
 
         return DataRow(self.header(), row, index=index, parent=self)
 
-    def insert_row(self, row_data):
+    def insert_row(self, row_data: Tuple[Any]) -> None:
         if len(row_data) != self.count_columns():
             raise IndexError("Number of data ({}) out of range".format(
                 len(row_data)))
@@ -136,11 +145,11 @@ class DataTable(object):
         sql = "INSERT INTO {} VALUES {}".format(self.name, tuple(row_data))
         self.parent.query(sql)
 
-    def insert_rows(self, rows_data):
+    def insert_rows(self, rows_data: List[Tuple[Any]]) -> None:
         for row_data in rows_data:
             self.insert_row(row_data)
 
-    def delete_row(self, index):
+    def delete_row(self, index: int) -> None:
         if index < 0:
             index = self.max_row + index
 
@@ -149,15 +158,15 @@ class DataTable(object):
         sql = "DELETE FROM {} WHERE rowid={}".format(self.name, rowid)
         self.parent.query(sql)
 
-    def delete_rows(self, indexes):
+    def delete_rows(self, indexes: int) -> None:
         for index in indexes:
             self.delete_row(index)
 
-    def delete_table(self):
+    def delete_table(self) -> None:
         sql = "DELETE FROM {}".format(self.name)
         self.parent.query(sql)
 
-    def delete_where(self, deleting_items):
+    def delete_where(self, deleting_items: List[Union[Any]]) -> None:
         sql = "DELETE FROM {} WHERE ".format(self.name)
 
         for item in deleting_items:
@@ -173,14 +182,8 @@ class DataTable(object):
 
         self.parent.query(sql)
 
-    def _create(self):
-        cols = [str(col) + " " + type for col, type in zip(self.header(), self.htypes())]
-        joined_cols = ", ".join(cols)
-        sql = "CREATE TABLE {}({})".format(self._name, joined_cols)
-        self.parent.query(sql)
-
-    def head(self, max_rows=5, with_rowid=False):
-        return self._to_string(with_rowid, max_rows)
+    def head(self, max_rows: int = 5, with_rowid: bool = False) -> str:
+        return self._to_string(max_rows, with_rowid)
 
     def attributes(self):
         print(f"___DataTable___\n"
@@ -194,23 +197,17 @@ class DataTable(object):
               f"Number of columns:  {self.count_columns()}\n"
               f"Number of rows:     {self.count_rows()}")
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-
-    def __repr__(self):
-        return "DataTable(name={})".format(self.name)
+    def _create(self) -> None:
+        cols = [str(col) + " " + type for col, type in zip(self.header(), self.htypes())]
+        joined_cols = ", ".join(cols)
+        sql = "CREATE TABLE {}({})".format(self._name, joined_cols)
+        self.parent.query(sql)
 
     def _get_table(self):
         sql = "SELECT rowid, * FROM {}".format(self.name)
-        # self.parent.query(sql)
-
         return self.parent.query(sql)
-        # return self.parent.fetchall()
 
-    def _to_string(self, with_rowid=False, max_rows=None):
+    def _to_string(self, max_rows: int = None, with_rowid: bool = False) -> str:
         result = []
 
         max = self.count_rows()
@@ -234,9 +231,9 @@ class DataTable(object):
 
         return str("\n".join(result))
 
-    def _width(self, table):
-        transpo_table = [list(x) for x in zip(*table)]
+    def _width(self, table_data: List[Any]) -> List[int]:
+        transpo_table = [list(x) for x in zip(*table_data)]
         return [self._column_width(col) for col in transpo_table]
 
-    def _column_width(self, column):
+    def _column_width(self, column: List[Any]) -> int:
         return max(len(str(row)) for row in column)
